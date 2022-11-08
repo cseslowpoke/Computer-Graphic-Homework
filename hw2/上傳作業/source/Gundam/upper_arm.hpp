341f85bfd4bb0b467d806a6e857fe67a72c9e7e7
cseslowpoke <cseslowpoke@gmail.com>
2022-11-08 15:23:52 +0800

    fin

--- hw2/上傳作業/source/Gundam/upper_arm.hpp
+++ hw2/上傳作業/source/Gundam/upper_arm.hpp
@@ -1,17 +1,21 @@
+
 #include <GL/glut.h>
 #include <cmath>
+
 namespace gundam {
-class thigh {
+class upper_arm {
   GLUquadricObj *cylinder = gluNewQuadric();
   GLUquadricObj *sphere = gluNewQuadric();
 
 public:
   void draw() {
     glPushMatrix();
+    glColor3d(0.07450980392, 0.39215686274, 0.31764705882);
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    glRotatef(-90, 1, 0, 0);
+    gluSphere(sphere, 1, 10, 10);
     glColor3d(0.74117647058, 0.78823529411, 0.78823529411);
-    gluCylinder(cylinder, 1, 1, 4, 10, 10);
+    gluCylinder(cylinder, 0.6, 0.6, 2.3, 10, 10);
+
     glPopMatrix();
   }
 };
